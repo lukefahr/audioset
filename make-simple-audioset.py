32af86d9e028a721388b5e58f6642a08471856ca
Andrew Lukefahr <lukefahr@indiana.edu>
2018-08-15 17:38:24 -0400

    updates for simple example

--- make-simple-audioset.py
+++ make-simple-audioset.py
@@ -5,13 +5,26 @@ cwd = os.getcwd()
 trucks    = cwd + '/data/trucks'
 no_trucks = cwd + '/data/no_trucks'
 
-audioset.GoodAudioSetData(num_samples=10, 
-                          download=True,
-                          framerate=22000, 
-                          max_threads = 4,
-                          data_dir=trucks)
-audioset.BadAudioSetData(num_samples=10, 
-                         download=True,
-                         framerate=22000, 
-                         max_threads = 4,
-                         data_dir=no_trucks)
+asb = audioset.AudioSetBuilder()
+
+good_clips = asb.getClips( includes= ['Truck', 'Medium engine (mid frequency)', ], 
+                  excludes=[ 'Air brake', 
+                                'Air horn, truck horn', 
+                                'Reversing beeps', 
+                                'Ice cream truck, ice cream van', 
+                                'Fire engine, fire truck (siren)',
+                                'Jet engine', 
+                               ], 
+                    num_clips = 10, download=True, max_threads=5) 
+print ('Good Clips: ')
+for clip in good_clips:
+    print ('\t ' + str(clip))
+
+
+bad_clips = asb.getClips ( includes = ['all'],
+                excludes =[ 'Truck', 'Medium engine (mid frequency)', 'Idling'],
+                num_clips = 10, download=True, max_threads = 5)
+
+print ('Bad Clips: ')
+for clip in bad_clips:
+    print ('\t ' + str(clip))
